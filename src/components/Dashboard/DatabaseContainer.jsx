a6381c0481dff35d7c7bd4ad61872163bdfe4d8d
Armando Peralta - XxTavo16xX <tavo14169@gmail.com>
2022-12-14 02:45:39 -0600

    Build: Database Window Script Complete.
    In this commit we populate the Window with the Data recived by
    Datasync-API.

--- src/components/Dashboard/DatabaseContainer.jsx
+++ src/components/Dashboard/DatabaseContainer.jsx
@@ -8,31 +8,31 @@ const databaseConecctionData = [
     {
         lastDatabaseChangeDate: 1670329001,
         databaseName: "Ordenes",
-        lastDatabaseUsersChangePhotoURL: ['./src/assets/components/SIdeNavBar/me.jpg', './src/assets/components/SIdeNavBar/oskar.jpg', './src/assets/components/SIdeNavBar/defaultUser.png'],
+        lastDatabaseUsersChangePhotoURL: ['/src/assets/components/TopNavBar/me.jpg', '/src/assets/components/TopNavBar/oskar.jpg', '/src/assets/components/TopNavBar/defaultUser.png'],
         databaseStoreSpace: "15 MB"
     },
     {
         lastDatabaseChangeDate: 1670329001,
         databaseName: "Cobranza",
-        lastDatabaseUsersChangePhotoURL: ['./src/assets/components/SIdeNavBar/me.jpg', './src/assets/components/SIdeNavBar/oskar.jpg', './src/assets/components/SIdeNavBar/defaultUser.png'],
+        lastDatabaseUsersChangePhotoURL: ['/src/assets/components/TopNavBar/me.jpg', '/src/assets/components/TopNavBar/oskar.jpg', '/src/assets/components/TopNavBar/defaultUser.png'],
         databaseStoreSpace: "15 MB"
     },
     {
         lastDatabaseChangeDate: 1670329001,
         databaseName: "Catalogo",
-        lastDatabaseUsersChangePhotoURL: ['./src/assets/components/SIdeNavBar/me.jpg', './src/assets/components/SIdeNavBar/oskar.jpg', './src/assets/components/SIdeNavBar/defaultUser.png'],
+        lastDatabaseUsersChangePhotoURL: ['/src/assets/components/TopNavBar/me.jpg', '/src/assets/components/TopNavBar/oskar.jpg', '/src/assets/components/TopNavBar/defaultUser.png'],
         databaseStoreSpace: "15 MB"
     },
     {
         lastDatabaseChangeDate: 1670329001,
         databaseName: "Presupuesto",
-        lastDatabaseUsersChangePhotoURL: ['./src/assets/components/SIdeNavBar/me.jpg', './src/assets/components/SIdeNavBar/oskar.jpg', './src/assets/components/SIdeNavBar/defaultUser.png'],
+        lastDatabaseUsersChangePhotoURL: ['/src/assets/components/TopNavBar/me.jpg', '/src/assets/components/TopNavBar/oskar.jpg', '/src/assets/components/TopNavBar/defaultUser.png'],
         databaseStoreSpace: "15 MB"
     },
     {
         lastDatabaseChangeDate: 1670329001,
         databaseName: "Ventas",
-        lastDatabaseUsersChangePhotoURL: ['./src/assets/components/SIdeNavBar/me.jpg', './src/assets/components/SIdeNavBar/oskar.jpg', './src/assets/components/SIdeNavBar/defaultUser.png'],
+        lastDatabaseUsersChangePhotoURL: ['./src/assets/components/SIdeNavBar/me.jpg', './src/assets/components/SIdeNavBar/oskar.jpg', '/src/assets/components/TopNavBar/defaultUser.png'],
         databaseStoreSpace: "15 MB"
     },
     {
@@ -54,357 +54,85 @@ function DatabaseContainer() {
 
             <div className="Database-Windows-Container">
 
-                <div className="Database-Windows-Content-Margin">
+                <div className="Database-Windows-Content-Margin">{
 
-                    <div className="Database-Window-Container">
+                    // * Populating Database-Windows-Container with the Data recived by Datasync-API
 
-                        <div className="Database-Window-Content-Margin">
+                    databaseConecctionData.map(({ lastDatabaseChangeDate, databaseName, lastDatabaseUsersChangePhotoURL, databaseStoreSpace }) => {
 
-                            <div className="Database-Window-Top-Container">
+                        return <div className="Database-Window-Container" key={databaseName}>
 
-                                <div className="Database-Window-Last-Date-User-Container">
+                            <div className="Database-Window-Content-Margin">
 
-                                    <p className="Database-Window-Last-Date-User-Label"> Diciembre 16 2022 </p>
+                                <div className="Database-Window-Top-Container">
 
-                                </div>
+                                    <div className="Database-Window-Last-Date-User-Container">
 
-                                <button className="Database-Window-Options-Button">
-
-                                    <MdMoreVert size={16} color={'#000000'} />
-
-                                </button>
-
-                            </div>
-
-                            <div className="Database-Window-Center-Container">
-
-                                <p className="Database-Window-Center-Name-Label">Ordenes</p>
-
-                            </div>
-
-                            <div className="Database-Window-Bottom-Container">
-
-                                <div className="Database-Window-Last-Users-Container">
-
-                                    <div className="Database-Window-User-Snippet">
-
-                                        <div className="Database-Window-User-Image-Container">
-
-                                            <img className='Databa-Window-User-Image' src="/src/assets/components/TopNavBar/me.jpg" />
-
-                                        </div>
-
-                                        <div className="Database-Window-User-Image-Container">
-
-                                            <img className='Databa-Window-User-Image' src="/src/assets/components/TopNavBar/oskar.jpg" />
-
-                                        </div>
-
-                                        <div className="Database-Window-User-Image-Container">
-
-                                            <img className='Databa-Window-User-Image' src="/src/assets/components/TopNavBar/defaultUser.png" />
-
-                                        </div>
-
+                                        <p className="Database-Window-Last-Date-User-Label">{new Date(lastDatabaseChangeDate * 1000).toLocaleDateString('SP-MX').replaceAll('/', '-')}</p>
 
                                     </div>
 
+                                    <button className="Database-Window-Options-Button">
+
+                                        <MdMoreVert size={16} color={'#000000'} />
+
+                                    </button>
+
                                 </div>
 
-                                <div className="Database-Window-Storage-Space-Container">
+                                <div className="Database-Window-Center-Container">
 
-                                    <div className="Database-Window-Storage-Space-Snippet">
+                                    <p className="Database-Window-Center-Name-Label">{databaseName}</p>
 
-                                        <p className="Database-Window-Storage-Space-Label">2 GB</p>
+                                </div>
+
+                                <div className="Database-Window-Bottom-Container">
+
+                                    <div className="Database-Window-Last-Users-Container">
+
+                                        <div className="Database-Window-User-Snippet">
+
+                                            {
+
+                                                lastDatabaseUsersChangePhotoURL.map((currentUserImageURL) => {
+
+                                                    return <div className="Database-Window-User-Image-Container">
+
+                                                        <img className='Databa-Window-User-Image' src={currentUserImageURL} />
+
+                                                    </div>
+
+                                                })
+
+                                            }
+
+
+                                        </div>
 
                                     </div>
 
+                                    <div className="Database-Window-Storage-Space-Container">
+
+                                        <div className="Database-Window-Storage-Space-Snippet">
+
+                                            <p className="Database-Window-Storage-Space-Label">{databaseStoreSpace}</p>
+
+                                        </div>
+
+                                    </div>
+
+
                                 </div>
 
-
                             </div>
 
                         </div>
 
-                    </div>
+                    })
 
-
-
-                </div>
-
-                {/*
-
-                <div className="databaseShortCutContainer">
-
-                    <div className="databaseTopContainer">
-
-                        <p className='databaseLastModDate'>06, Diciembre, 2022</p>
-
-                        <MdMoreVert size={15} />
-
-                    </div>
-
-                    <div className="databaseContentContainer">
-
-                        <p className='databaseName'>Ordenes</p>
-
-                    </div>
-
-                    <div className="databaseBottomContainer">
-
-                        <div className="lastModificationFromUsersContainer">
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/me.jpg"/>
-
-                            </div>
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/oskar.jpg"/>
-
-                            </div>
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/defaultUser.png"/>
-
-                            </div>
-
-
-                        </div>
-
-                    </div>
+                }
 
                 </div>
-
-                <div className="databaseShortCutContainer">
-
-                    <div className="databaseTopContainer">
-
-                        <p className='databaseLastModDate'>02, Diciembre, 2022</p>
-
-                        <MdMoreVert size={15} />
-
-                    </div>
-
-                    <div className="databaseContentContainer">
-
-                        <p className='databaseName'>Facturas</p>
-
-                    </div>
-
-                    <div className="databaseBottomContainer">
-
-                        <div className="lastModificationFromUsersContainer">
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/me.jpg"/>
-
-                            </div>
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/oskar.jpg"/>
-
-                            </div>
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/defaultUser.png"/>
-
-                            </div>
-
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div className="databaseShortCutContainer">
-
-                    <div className="databaseTopContainer">
-
-                        <p className='databaseLastModDate'>07, Diciembre, 2022</p>
-
-                        <MdMoreVert size={15} />
-
-                    </div>
-
-                    <div className="databaseContentContainer">
-
-                        <p className='databaseName'>Servicios</p>
-
-                    </div>
-
-                    <div className="databaseBottomContainer">
-
-                        <div className="lastModificationFromUsersContainer">
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/me.jpg"/>
-
-                            </div>
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/oskar.jpg"/>
-
-                            </div>
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/defaultUser.png"/>
-
-                            </div>
-
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div className="databaseShortCutContainer">
-
-                    <div className="databaseTopContainer">
-
-                        <p className='databaseLastModDate'>06, Diciembre, 2022</p>
-
-                        <MdMoreVert size={15} />
-
-                    </div>
-
-                    <div className="databaseContentContainer">
-
-                        <p className='databaseName'>Ordenes</p>
-
-                    </div>
-
-                    <div className="databaseBottomContainer">
-
-                        <div className="lastModificationFromUsersContainer">
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/me.jpg"/>
-
-                            </div>
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/oskar.jpg"/>
-
-                            </div>
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/defaultUser.png"/>
-
-                            </div>
-
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div className="databaseShortCutContainer">
-
-                    <div className="databaseTopContainer">
-
-                        <p className='databaseLastModDate'>02, Diciembre, 2022</p>
-
-                        <MdMoreVert size={15} />
-
-                    </div>
-
-                    <div className="databaseContentContainer">
-
-                        <p className='databaseName'>Facturas</p>
-
-                    </div>
-
-                    <div className="databaseBottomContainer">
-
-                        <div className="lastModificationFromUsersContainer">
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/me.jpg"/>
-
-                            </div>
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/oskar.jpg"/>
-
-                            </div>
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/defaultUser.png"/>
-
-                            </div>
-
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div className="databaseShortCutContainer">
-
-                    <div className="databaseTopContainer">
-
-                        <p className='databaseLastModDate'>07, Diciembre, 2022</p>
-
-                        <MdMoreVert size={15} />
-
-                    </div>
-
-                    <div className="databaseContentContainer">
-
-                        <p className='databaseName'>Servicios</p>
-
-                    </div>
-
-                    <div className="databaseBottomContainer">
-
-                        <div className="lastModificationFromUsersContainer">
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/me.jpg"/>
-
-                            </div>
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/oskar.jpg"/>
-
-                            </div>
-
-                            <div className="lastUserModificationImageContainer">
-
-                                <img src="./src/assets/components/SIdeNavBar/defaultUser.png"/>
-
-                            </div>
-
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-                */}
 
             </div>
 
