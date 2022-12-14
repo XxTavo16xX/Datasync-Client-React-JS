40572b31e9a50051bf262ac31549361aeb4b52a3
Armando Peralta - XxTavo16xX <tavo14169@gmail.com>
2022-12-14 13:27:41 -0600

    Refactor: Pending Task COmponent refatored.

--- src/components/Dashboard/PendingTaskContainer.jsx
+++ src/components/Dashboard/PendingTaskContainer.jsx
@@ -1,6 +1,17 @@
 import { MdMic, MdSend } from 'react-icons/md'
 import './styles/PendingTask.css'
 
+const pendingTaskReceived = [
+    {
+        pendingTaskName: 'Enviar Factura a ...',
+        isPendingTaskCompleted: true
+    },{
+        pendingTaskName: 'Enviar cotizacion a ...',
+        isPendingTaskCompleted: false
+    },
+
+]
+
 function PendingTasksContainer() {
 
     return <div className="Pending-Task-Content-Container">
@@ -9,7 +20,31 @@ function PendingTasksContainer() {
 
             <p className='Pending-Task-Content-Title-Label'>Tareas Pendientes</p>
 
-            <div className="Pending-Task-List-Container"></div>
+            <div className="Pending-Task-List-Container">
+
+                {pendingTaskReceived.length == 0 ? <p className="Pending-Task-List-Empty-Message">Aun no has agregado tareas pendientes.</p> : null}
+
+                {
+
+                    pendingTaskReceived.map(({pendingTaskName, isPendingTaskCompleted}) => {
+
+                        return <div className="Pending-Task-Container" key={pendingTaskName}>
+
+                            <div className="Pending-Task-Action-Input-Container">
+
+                                <input className='Pending-Task-Action-Input-Container-CheckBox' type="checkbox" name="" id={pendingTaskName} />
+
+                            </div>
+
+                            <p className="Pending-Task-Name-Label">{pendingTaskName}</p>
+
+                        </div>
+
+                    })
+
+                }
+
+            </div>
 
             <div className="Pending-Task-Input-Container">
 
@@ -21,7 +56,7 @@ function PendingTasksContainer() {
 
                 <div className="Pending-Task-Input-Container-Bar">
 
-                    <input className='Pending-Task-Input-Container-Input' type="text" placeholder='Enviar correo a ...'/>
+                    <input className='Pending-Task-Input-Container-Input' type="text" placeholder='Enviar correo a ...' />
 
                 </div>
 
