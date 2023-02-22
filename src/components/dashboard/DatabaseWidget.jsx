49bc1423ad47dccd6bf3f5d27f1fb0225a95d85d
Armando Peralta - XxTavo16xX <tavo14169@gmail.com>
2023-02-22 06:09:31 -0600

    Build: Database Nodes Empty Container.

--- src/components/dashboard/DatabaseWidget.jsx
+++ src/components/dashboard/DatabaseWidget.jsx
@@ -3,65 +3,17 @@
 
 import { useContext } from "react";
 import { MdMoreVert } from 'react-icons/md'
+import Lottie from "lottie-react";
 
 // * Modules Required
 
 import { AppContext } from '../../app/Context';
 import { getDateInHumanFormatByTimestamp } from "../../lib/Calendar";
+import emptyBoxAnimation from '../../assets/animations/emptyBoxAnimation.json'
 
 // * view Styles
 
 import './styles/DatabaseWidget.css'
-
-// * Fetched data example 
-// ! Must replace with data received by module
-
-const databaseConecctionData = [
-
-    {
-        lastDatabaseChangeDate: 1675068318,
-        databaseName: "DevLabel",
-        databaseKey: 1,
-        lastDatabaseUsersChangePhotoURL: ['https://scontent.webdesignnodes.com/datasync_dev/images/muffin.png', 'https://scontent.webdesignnodes.com/datasync_dev/images/me.jpg', 'https://scontent.webdesignnodes.com/datasync_dev/images/elon.png'],
-        databaseStoreSpace: "15 MB"
-    },
-    {
-        lastDatabaseChangeDate: 1674682258,
-        databaseName: "DevLabel",
-        databaseKey: 2,
-        lastDatabaseUsersChangePhotoURL: ['https://scontent.webdesignnodes.com/datasync_dev/images/elon.png', 'https://scontent.webdesignnodes.com/datasync_dev/images/me.jpg', 'https://scontent.webdesignnodes.com/datasync_dev/images/muffin.png'],
-        databaseStoreSpace: "13.3 MB"
-    },
-    {
-        lastDatabaseChangeDate: 1674595858,
-        databaseName: "DevLabel",
-        databaseKey: 3,
-        lastDatabaseUsersChangePhotoURL: ['https://scontent.webdesignnodes.com/datasync_dev/images/me.jpg', 'https://scontent.webdesignnodes.com/datasync_dev/images/muffin.png', 'https://scontent.webdesignnodes.com/datasync_dev/images/elon.png'],
-        databaseStoreSpace: "121 MB"
-    },
-    {
-        lastDatabaseChangeDate: 1670323001,
-        databaseName: "DevLabel",
-        databaseKey: 4,
-        lastDatabaseUsersChangePhotoURL: ['https://scontent.webdesignnodes.com/datasync_dev/images/muffin.png', 'https://scontent.webdesignnodes.com/datasync_dev/images/elon.png', 'https://scontent.webdesignnodes.com/datasync_dev/images/me.jpg'],
-        databaseStoreSpace: "19.3 MB"
-    },
-    {
-        lastDatabaseChangeDate: 1661459458,
-        databaseName: "DevLabel",
-        databaseKey: 5,
-        lastDatabaseUsersChangePhotoURL: ['https://scontent.webdesignnodes.com/datasync_dev/images/elon.png', 'https://scontent.webdesignnodes.com/datasync_dev/images/me.jpg', 'https://scontent.webdesignnodes.com/datasync_dev/images/muffin.png'],
-        databaseStoreSpace: "10.7 MB"
-    },
-    {
-        lastDatabaseChangeDate: 1656189058,
-        databaseName: "DevLabel",
-        databaseKey: 6,
-        lastDatabaseUsersChangePhotoURL: ['https://scontent.webdesignnodes.com/datasync_dev/images/me.jpg', 'https://scontent.webdesignnodes.com/datasync_dev/images/elon.png', 'https://scontent.webdesignnodes.com/datasync_dev/images/muffin.png'],
-        databaseStoreSpace: "15 MB"
-    }
-
-]
 
 // * view to Return
 
@@ -77,83 +29,113 @@ const DatabaseWidget = ({ databaseNodes }) => {
 
             <div className="Database-Windows-Container">
 
-                <div className="Database-Windows-Content-Margin">{
+                <div className={databaseNodes.length === 0 ? "Database-Windows-Empty-Content-Margin" : "Database-Windows-Content-Margin"}>
 
-                    // * Populating Database-Windows-Container with the Data recived by Datasync-API
+                    {
 
-                    databaseNodes.map((currentDBNode, index) => {
+                        databaseNodes.length === 0 ?
 
-                        return <div className="Database-Window-Container" key={index} onClick={() => { goDatabaseViewInCollection(currentDBNode.dbName) }}>
+                            <div className="Database-Nodes-Empty-Message-Container">
 
-                            <div className="Database-Window-Content-Margin">
+                                <div className="Database-Nodes-Empty-Animation-Container">
 
-                                <div className="Database-Window-Top-Container">
-
-                                    <div className="Database-Window-Last-Date-User-Container">
-
-                                        <p className="Database-Window-Last-Date-User-Label">{getDateInHumanFormatByTimestamp(currentDBNode.dbUpdatedAt)}</p>
-
-                                    </div>
-
-                                    <div className="Database-Window-Options-Button">
-
-                                        <MdMoreVert size={16} color={'#000d41'} />
-
-                                    </div>
+                                    <Lottie animationData={emptyBoxAnimation} loop={true} />
 
                                 </div>
 
-                                <div className="Database-Window-Center-Container">
+                                <p className="Database-Nodes-Empty-Message-Label">Aun no haz creado ninguna base de datos</p>
 
-                                    <p className="Database-Window-Center-Name-Label">{currentDBNode.dbName}</p>
+                                <div className="Database-Nodes-Empty-Create-Database-Button">
+
+                                    <p className="Create-Database-Button-Label">Crear base de datos</p>
 
                                 </div>
 
-                                <div className="Database-Window-Bottom-Container">
+                                <div className="Database-Nodes-Empty-Report-Error-Button">
 
-                                    <div className="Database-Window-Last-Users-Container">
-
-                                        <div className="Database-Window-User-Snippet">
-
-                                            {
-
-                                                currentDBNode.lastUpdateUser.map((currentUser, index) => {
-
-                                                    return <div className="Database-Window-User-Image-Container" key={index} >
-
-                                                        <img className='Databa-Window-User-Image' src={currentUser.userPhotoProfile} />
-
-                                                    </div>
-
-                                                })
-
-                                            }
-
-
-                                        </div>
-
-                                    </div>
-
-                                    <div className="Database-Window-Storage-Space-Container">
-
-                                        <div className="Database-Window-Storage-Space-Snippet">
-
-                                            <p className="Database-Window-Storage-Space-Label">{currentDBNode.dbName}</p>
-
-                                        </div>
-
-                                    </div>
-
+                                    <p className="Create-Database-Error-Button-Label">Reportar problema</p>
 
                                 </div>
 
                             </div>
 
-                        </div>
+                            :
 
-                    })
+                            // * Populating Database-Windows-Container with the Data recived by Datasync-API
 
-                }
+                            databaseNodes.map((currentDBNode, index) => {
+
+                                return <div className="Database-Window-Container" key={index} onClick={() => { goDatabaseViewInCollection(currentDBNode.dbName) }}>
+
+                                    <div className="Database-Window-Content-Margin">
+
+                                        <div className="Database-Window-Top-Container">
+
+                                            <div className="Database-Window-Last-Date-User-Container">
+
+                                                <p className="Database-Window-Last-Date-User-Label">{getDateInHumanFormatByTimestamp(currentDBNode.dbUpdatedAt)}</p>
+
+                                            </div>
+
+                                            <div className="Database-Window-Options-Button">
+
+                                                <MdMoreVert size={16} color={'#000d41'} />
+
+                                            </div>
+
+                                        </div>
+
+                                        <div className="Database-Window-Center-Container">
+
+                                            <p className="Database-Window-Center-Name-Label">{currentDBNode.dbName}</p>
+
+                                        </div>
+
+                                        <div className="Database-Window-Bottom-Container">
+
+                                            <div className="Database-Window-Last-Users-Container">
+
+                                                <div className="Database-Window-User-Snippet">
+
+                                                    {
+
+                                                        currentDBNode.lastUpdateUser.map((currentUser, index) => {
+
+                                                            return <div className="Database-Window-User-Image-Container" key={index} >
+
+                                                                <img className='Databa-Window-User-Image' src={currentUser.userPhotoProfile} />
+
+                                                            </div>
+
+                                                        })
+
+                                                    }
+
+
+                                                </div>
+
+                                            </div>
+
+                                            <div className="Database-Window-Storage-Space-Container">
+
+                                                <div className="Database-Window-Storage-Space-Snippet">
+
+                                                    <p className="Database-Window-Storage-Space-Label">{currentDBNode.dbName}</p>
+
+                                                </div>
+
+                                            </div>
+
+
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                            })
+
+                    }
 
                 </div>
 
